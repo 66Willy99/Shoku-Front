e9cf92f167e8533d83a756d9ebd4b30d67ecbba5
Jeremy <jeremyvillanuevam@gmail.com>
2025-06-05 00:50:32 -0400

    Create StyleSheet trabajadores

--- app/admin/index.tsx
+++ app/admin/index.tsx
@@ -7,114 +7,125 @@ import { useAuth } from '../../context/authContext';
 import BoldText from '@/components/ui/CustomText';
 import { saveSession } from '../../services/sessionService'; 
 import { Config } from '../../constants/config';
+import LoadingScreen from '@/components/ui/LoadingScreen';
 
 
 const LoginScreen = () => {
-const router = useRouter();
-const { login } = useAuth();
-const [email, setEmail] = useState<string>('');
-const [password, setPassword] = useState<string>('');
+    const router = useRouter();
+    const { login } = useAuth();
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-const handleLogin = async () => {
-    if (!email.trim() || !password.trim()) {
-        Alert.alert('Error', 'Por favor, completa todos los campos');
-        console.log('Error', 'Por favor, completa todos los campos')
-        return;
-    }
 
-    if (!/^\S+@\S+\.\S+$/.test(email)) {
-        Alert.alert('Error', 'Ingresa un correo electrónico válido');
-        console.log('Error', 'Ingresa un correo electrónico válido')
-        return;
-    }
-
-    try {
-        // 1. Primero hacemos login para obtener el UID
-        const loginResponse = await fetch(`${Config.API_URL}/user/auth`, {
-            method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                email: email.trim(),
-                password: password.trim(),
-            }),
-        });
-
-        const loginData = await loginResponse.json();
-
-        if (!loginResponse.ok) {
-            throw new Error(loginData.detail || 'Error en el inicio de sesión');
+    const handleLogin = async () => {
+        if (!email.trim() || !password.trim()) {
+            Alert.alert('Error', 'Por favor, completa todos los campos');
+            console.log('Error', 'Por favor, completa todos los campos')
+            return;
         }
 
-        console.log('Login exitoso, UID:', loginData.localId);
-
-        const restaurantesResponse = await fetch(`${Config.API_URL}/restaurants/?user_id=${loginData.localId}`);
-        
-        if (!restaurantesResponse.ok) {
-            throw new Error('Error al obtener restaurantes');
+        if (!/^\S+@\S+\.\S+$/.test(email)) {
+            Alert.alert('Error', 'Ingresa un correo electrónico válido');
+            console.log('Error', 'Ingresa un correo electrónico válido')
+            return;
         }
 
-        const restaurantes = await restaurantesResponse.json();
-        
-        // Extraer solo los IDs (asumiendo que la API devuelve objeto con IDs como claves)
-        const restaurantIds = Object.keys(restaurantes);
+        setIsSubmitting(true);
 
-        console.log('Restaurantes:', restaurantIds);
+        try {
+            // 1. Primero hacemos login para obtener el UID
+            const loginResponse = await fetch(`${Config.API_URL}/user/auth`, {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: email.trim(),
+                    password: password.trim(),
+                }),
+            });
 
-        await login(loginData.localId, restaurantIds[0] || undefined);
+            const loginData = await loginResponse.json();
 
-        // Guardamos los datos importantes
-        await saveSession({
-            token: loginData.idToken,
-            uid: loginData.localId,
-            restaurantIds // Enviamos el array completo
-        });
-        
+            if (!loginResponse.ok) {
+                throw new Error(loginData.detail || 'Error en el inicio de sesión');
+            }
 
-    } catch (error: any) {
-    console.error('Error completo:', error);
-    Alert.alert(
-        'Error', 
-        error.message || 'Ocurrió un error durante el proceso'
-    );
+            console.log('Login exitoso, UID:', loginData.localId);
+
+            const restaurantesResponse = await fetch(`${Config.API_URL}/restaurants/?user_id=${loginData.localId}`);
+            
+            if (!restaurantesResponse.ok) {
+                throw new Error('Error al obtener restaurantes');
+            }
+
+            const restaurantes = await restaurantesResponse.json();
+            
+            // Extraer solo los IDs (asumiendo que la API devuelve objeto con IDs como claves)
+            const restaurantIds = Object.keys(restaurantes);
+
+            console.log('Restaurantes:', restaurantIds);
+
+            await login(loginData.localId, restaurantIds[0] || undefined);
+
+            // Guardamos los datos importantes
+            await saveSession({
+                token: loginData.idToken,
+                uid: loginData.localId,
+                restaurantIds // Enviamos el array completo
+            });
+            
+
+        } catch (error: any) {
+            console.error('Error completo:', error);
+            Alert.alert(
+                'Error', 
+                error.message || 'Ocurrió un error durante el proceso'
+            );
+        }finally{
+            setIsSubmitting(false);
+        }
+    };
+
+    if(isSubmitting){
+        return(<LoadingScreen message='Iniciando sesion'/>);
     }
-};
 
-return (
-    <View style={styles.container}>
-        <View style={styles.primarySection}>
-            <Icon color={Colors.light_primary} size={300} />
-            <View style={{marginTop: 100 }}>
-            <BoldText style={styles.titleText}>Shoku - 1.0.0</BoldText>
-            <BoldText style={styles.titleText}>Gestor de restaurante</BoldText>
+    return (
+        <View style={styles.container}>
+            <View style={styles.primarySection}>
+                <Icon color={Colors.light_primary} size={300} />
+                <View style={{marginTop: 100 }}>
+                <BoldText style={styles.titleText}>Shoku - 1.0.0</BoldText>
+                <BoldText style={styles.titleText}>Gestor de restaurante</BoldText>
+                </View>
+            </View>
+
+            <View style={styles.secondarySection}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Correo electrónico"
+                    value={email}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                />
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Contraseña"
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                />
+
+                <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                    <Text style={styles.buttonText}>Ingresar</Text>
+                </TouchableOpacity>
             </View>
         </View>
-
-        <View style={styles.secondarySection}>
-            <TextInput
-                style={styles.input}
-                placeholder="Correo electrónico"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                autoCapitalize="none"
-            />
-
-            <TextInput
-                style={styles.input}
-                placeholder="Contraseña"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-
-            <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                <Text style={styles.buttonText}>Ingresar</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-);
+    );
 };
 
 const styles = StyleSheet.create({
