ddcea9e03d1f21f7790dc32565c11df03e638c21
Guillermo Adrian Mercado Castillo <133931931+66Willy99@users.noreply.github.com>
2025-07-03 13:27:39 -0400

    feat: Simplify app configuration by removing unused platform and notification settings

--- app.config.js
+++ app.config.js
@@ -5,67 +5,8 @@ export default {
     name: 'ShokuApp',
     slug: 'shoku-app',
     version: '1.0.0',
-    platforms: ['ios', 'android', 'web'],
-    orientation: 'portrait',
-    icon: './assets/images/icon.png',
-    userInterfaceStyle: 'light',
-    splash: {
-      image: './assets/images/splash-icon.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff'
-    },
-    ios: {
-      supportsTablet: true
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: './assets/images/adaptive-icon.png',
-        backgroundColor: '#ffffff'
-      },
-      permissions: [
-        'CAMERA',
-        'RECORD_AUDIO',
-        'RECEIVE_BOOT_COMPLETED',
-        'VIBRATE',
-        'com.android.vending.BILLING',
-        'NOTIFICATIONS',
-        'RECEIVE_BOOT_COMPLETED',
-        'WAKE_LOCK'
-      ]
-    },
-    web: {
-      bundler: 'metro',
-      output: 'static',
-      favicon: './assets/images/favicon.png'
-    },
-    plugins: [
-      'expo-router',
-      [
-        'expo-notifications',
-        {
-          icon: './assets/images/icon.png',
-          color: '#ffffff',
-          sounds: ['./assets/notification-sound.wav'],
-          mode: 'production'
-        }
-      ]
-    ],
-    notification: {
-      icon: './assets/images/icon.png',
-      color: '#ffffff',
-      androidMode: 'default',
-      androidCollapsedTitle: 'Shoku - Notificaciones',
-      iosDisplayInForeground: true,
-      vapidPublicKey: process.env.EXPO_VAPID_PUBLIC_KEY || 'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEgJMrze31pMBcupHtkgcoqknvgMFbG4_DIZrRpkUqOaih8RyPHyL_gPbxKFABj-KZdh4xblZr4MIWt_eCckN4hg'
-    },
-    experiments: {
-      typedRoutes: true
-    },
     extra: {
       API_URL: process.env.EXPO_PUBLIC_API_URL,
-      eas: {
-        projectId: process.env.EXPO_PROJECT_ID || 'your-project-id-here' // Reemplaza con tu project ID real
-      }
-    },
+      },
   },
 };
